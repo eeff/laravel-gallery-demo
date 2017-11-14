efe677491dccd0c425563b8266a4d0ce48a8d6ed
cai <cai@local>
2017-11-14 19:05:59 +0800

    Add album store action

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Album;
 
 class AlbumsController extends Controller
 {
@@ -18,6 +19,25 @@ class AlbumsController extends Controller
 
     public function store(Request $request)
     {
-    	//
+    	$this->validate($request, [
+    		'name' => 'required',
+    		'cover_img' => 'image|max:1999'
+    	]);
+
+    	$finfo = pathinfo($request->file('cover_img')->getClientOriginalName());
+    	$fn = $finfo['filename'] . "_" . time() . $finfo['extension'];
+    	
+    	$path = $request->file('cover_img')->storeAs('public/album_covers', $fn);
+
+    	$album = new Album;
+    	$album->name = $request->input('name');
+    	$album->description = $request->input('description');
+    	$album->cover_img = $fn;
+
+    	if ($album->save()) {
+    		return redirect('/albums')->with('success', 'Album Created');
+    	} else {
+    		return redirect('/albums')->withErrors(['Album creation failure']);
+    	}
     }
 }
